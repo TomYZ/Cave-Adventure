68e49c5690c375b925291ebcb06937d45434103e
weishiqi <shiqiwei@usc.edu>
2016-06-04 15:43:27 -0700

    no message

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -30,7 +30,7 @@ public class Rocket : MonoBehaviour {
 		}
 	}
 	void OnTriggerEnter2D(Collider2D target){
-		if (target.gameObject.tag != "Coin")
+		if (target.gameObject.tag != "coin")
 			Destroy (gameObject);
 	}
 }
