e9a73aac8cf444557ed1f3f5f80e6c01fce91a13
TomYZ <zheny@usc.edu>
2016-06-15 19:33:39 -0700

    compressed images

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -22,6 +22,7 @@ public class MainController : MonoBehaviour {
 	private float t;
 	private float velocity;
 
+	private int start = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -41,6 +42,8 @@ public class MainController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		if (start == 0)
+			return;
 		if (velocity != 0) {
 			if (Time.time - t >= 0.1) {
 				velocity = 0;
@@ -50,6 +53,9 @@ public class MainController : MonoBehaviour {
 			anim.SetFloat ("Speed", Mathf.Abs(velocity));
 	}
 
+	public void StartGame(){
+		start=1;
+	}
 	public void OnClickUp(){
 		velocity = 1;
 		t = Time.time;
