2cc0758690cd8b130200c1dfa0138f405fda8c18
TomYZ <zheny@usc.edu>
2016-06-18 18:01:24 -0700

    fix bugs

--- Assets/Scripts/Obstacles/Cannon.cs
+++ Assets/Scripts/Obstacles/Cannon.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Cannon :AbstractBehavior {
 
-	public float shootDelay = .5f;
+	public float shootDelay ;
 	public GameObject projectilePrefab;
 
 	private float timeElapsed = 0f;
@@ -15,7 +15,7 @@ public class Cannon :AbstractBehavior {
 
 			var canFire = true;
 
-			if(canFire && timeElapsed > shootDelay){
+			if(canFire && (timeElapsed > shootDelay)){
 				CreateProjectile(new Vector3 (transform.position.x-2f, transform.position.y+2f, transform.position.z));
 				timeElapsed = 0;
 			}
