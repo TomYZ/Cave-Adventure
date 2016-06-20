0efe944a3ac444f6e28b0bf88c2d67ba42aee532
Jaeho Lee <Jaeho@Jaehos-MacBook-Pro.local>
2016-06-20 16:16:19 -0700

    3 kinds of enemy

--- Assets/Scripts/Behaviors/EnemyOccurs.cs
+++ Assets/Scripts/Behaviors/EnemyOccurs.cs
@@ -3,26 +3,69 @@ using System.Collections;
 
 public class EnemyOccurs : MonoBehaviour {
 
-	private float start_time;
-	public GameObject projectilePrefab;
+	private float last_position,enemy_delay=10f,randomNum;
+	public GameObject Ghost_up_prefab,Ghost_left_prefab,Cannon_prefab;
 
 	// Use this for initialization
 	void Start () {
-		start_time = Time.time;
+		last_position = transform.position.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.time - start_time > 1) {
-			start_time = Time.time;
+		
 
-				CreateProjectile(new Vector3 (transform.position.x + 20f, transform.position.y, transform.position.z));
+		if (transform.position.x - last_position > enemy_delay) {
+			
+			randomNum=Random.Range (0, 2);
+			if (randomNum == 1) {
+				for (int i = 0; i < 3; i++) {
+					randomNum = Random.Range (0, 2);
+					if (randomNum == 1) {
+						CreateEnemyLeft (new Vector3 (transform.position.x + 20f, -3 + i * 3, transform.position.z));
+
+					}
+
+				}
+			}
+
 
 		}
+		if (transform.position.x - last_position > enemy_delay) {
+			
+			randomNum=Random.Range (0, 2);
+			if (randomNum == 1) {
+				
+				CreateEnemyUp (new Vector3 (transform.position.x + 20f, 1.5f, transform.position.z));
+			}
+
+		}
+		if (transform.position.x - last_position > enemy_delay) {
+			last_position = transform.position.x;
+			randomNum=Random.Range (0f, 1f);
+			if (randomNum <= 0.3) {
+
+				CreateEnemyCannon (new Vector3 (transform.position.x + 20f, -4f, transform.position.z));
+			}
+
+		}
+
+
 	}
 
-	public void CreateProjectile(Vector2 pos){
-		var clone = Instantiate (projectilePrefab, pos, Quaternion.identity) as GameObject;
+	public void CreateEnemyUp(Vector2 pos){
+		var clone_up = Instantiate (Ghost_up_prefab, pos, Quaternion.identity) as GameObject;
+		//var clone_left = Instantiate (Ghost_left_prefab, pos, Quaternion.identity) as GameObject;
 		//clone.transform.localScale = transform.localScale;
+
+	}
+	public void CreateEnemyLeft(Vector2 pos){
+		//var clone_up = Instantiate (Ghost_up_prefab, pos, Quaternion.identity) as GameObject;
+		var clone_left = Instantiate (Ghost_left_prefab, pos, Quaternion.identity) as GameObject;
+
+		//clone.transform.localScale = transform.localScale;
+	}
+	public void CreateEnemyCannon(Vector2 pos){
+		var cannon = Instantiate (Cannon_prefab, pos, Quaternion.identity) as GameObject;
 	}
 }
---
+++ Assets/Scripts/Obstacles/Cannon.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon :AbstractBehavior {
+
+	public float shootDelay,lifetime;
+	public GameObject projectilePrefab;
+
+	private float timeElapsed = 0f;
+
+	// Update is called once per frame
+	void OnBecameInvisible() {
+		Destroy (gameObject);
+	}
+	void Start(){
+		lifetime = Time.time;
+	}
+	void Update () {
+		if (Time.time - lifetime > 7f) {
+			Destroy (gameObject);
+
+		}
+		if (projectilePrefab != null) {
+			
+			var canFire = true;
+
+			if(canFire && (timeElapsed > shootDelay)){
+				CreateProjectile(new Vector3 (transform.position.x-2f, transform.position.y+2f, transform.position.z));
+				timeElapsed = 0;
+			}
+
+			timeElapsed += Time.deltaTime;
+		}
+	}
+
+
+
+	public void CreateProjectile(Vector2 pos){
+		var clone = Instantiate (projectilePrefab, pos, Quaternion.identity) as GameObject;
+		clone.transform.localScale = transform.localScale;
+	}
+}
